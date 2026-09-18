4791f02daa9d4a6dc1c8f385490e2e237fb08471
CRJL1001 <constantlescutprive2@gmail.com>
2026-09-18 12:53:57 +0200

    routes chatroom#index et login OK

--- ga.sh
+++ ga.sh
@@ -1,4 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+if ["$#" -eq 0]; then
+	echo "Usage : $0 \"message du commit\""
+	exit 1
+fi
+
+MESSAGE="$*"
+
+echo "Ajout des fichiers...."
 git add .
-git commit -m $1
+
+echo "Création du commit...."
+git commit -m "$MESSAGE"
+
+echo "Push vers le dépôt...."
 git push
+
+echo "Commit / Push OK"
 
